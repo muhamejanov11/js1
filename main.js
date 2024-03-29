7c4ff24783146cf7817118ee9b1bbf8c3b59cd7f
muhamejanov11 <muhamejanov.n@gmail.com>
2024-03-29 21:29:52 +0500

    done

---
+++ main.js
@@ -0,0 +1,6 @@
+let str = prompt('Enter your name')
+let str1 = prompt('Enter your surname')
+let num = prompt('Enter your age')
+let str2 = prompt('Enter your adress')
+let str3 = prompt('Enter your Favorite school subject')
+console.log(str + str1 + num + str2 + str3);
